46091dc15be094ec2f7c517b6ecb75327f321c32
moonbai <y15269029585@gmail.com>
2026-06-04 05:28:50 +0000

    Fix: Remove duplicate repository config, use PREFER_SETTINGS mode

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,14 +13,6 @@ plugins {
     id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://jitpack.io") }
-    }
-}
-
 tasks.register("clean", Delete::class) {
     delete(rootProject.layout.buildDirectory)
 }
